ecf31ffcee4eac661915e672a5601b95d312070c
xinhai12310 <18135082@qq.com>
2025-10-14 05:45:09 -0700

    整数类型

--- src/observer/common/type/integer_type.cpp
+++ src/observer/common/type/integer_type.cpp
@@ -14,58 +14,123 @@ See the Mulan PSL v2 for more details. */
 #include "common/type/integer_type.h"
 #include "common/value.h"
 
+/**
+ * @brief 比较两个数值的大小
+ * @param left 左边的操作数，必须是整数类型
+ * @param right 右边的操作数，可以是整数或浮点数类型
+ * @return 
+ *  -1 表示 left < right
+ *  0 表示 left = right
+ *  1 表示 left > right
+ *  INT32_MAX 表示未实现的比较
+ */
 int IntegerType::compare(const Value &left, const Value &right) const
 {
-  ASSERT(left.attr_type() == AttrType::INTS, "left type is not integer");
+  // 确保左边操作数是整数类型
+  ASSERT(left.attr_type() == AttrType::INTS, "left type is integer");
+  // 确保右边操作数是数值类型（整数或浮点数）
   ASSERT(right.attr_type() == AttrType::INTS || right.attr_type() == AttrType::FLOATS, "right type is not numeric");
+  
+  // 根据右边操作数的类型选择不同的比较方法
   if (right.attr_type() == AttrType::INTS) {
+    // 两个整数比较，使用整数比较函数
     return common::compare_int((void *)&left.value_.int_value_, (void *)&right.value_.int_value_);
   } else if (right.attr_type() == AttrType::FLOATS) {
+    // 整数与浮点数比较，先将整数转换为浮点数，再使用浮点数比较函数
     float left_val  = left.get_float();
     float right_val = right.get_float();
     return common::compare_float((void *)&left_val, (void *)&right_val);
   }
+  
+  // 其他情况返回未实现的比较结果
   return INT32_MAX;
 }
 
+/**
+ * @brief 将整数值转换为指定类型
+ * @param val 要转换的值，必须是整数类型
+ * @param type 目标类型
+ * @param result 用于存储转换结果
+ * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+ */
 RC IntegerType::cast_to(const Value &val, AttrType type, Value &result) const
 {
+  // 根据目标类型进行不同的转换
   switch (type) {
   case AttrType::FLOATS: {
+    // 转换为浮点数类型
     float float_value = val.get_int();
     result.set_float(float_value);
     return RC::SUCCESS;
   }
   default:
+    // 不支持的目标类型
     LOG_WARN("unsupported type %d", type);
     return RC::SCHEMA_FIELD_TYPE_MISMATCH;
   }
 }
 
+/**
+ * @brief 计算两个整数的和
+ * @param left 左边的操作数
+ * @param right 右边的操作数
+ * @param result 用于存储计算结果
+ * @return 执行结果，成功返回 RC::SUCCESS
+ */
 RC IntegerType::add(const Value &left, const Value &right, Value &result) const
 {
+  // 计算两个整数的和并设置结果
   result.set_int(left.get_int() + right.get_int());
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 计算两个整数的差
+ * @param left 左边的操作数
+ * @param right 右边的操作数
+ * @param result 用于存储计算结果
+ * @return 执行结果，成功返回 RC::SUCCESS
+ */
 RC IntegerType::subtract(const Value &left, const Value &right, Value &result) const
 {
+  // 计算两个整数的差并设置结果
   result.set_int(left.get_int() - right.get_int());
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 计算两个整数的积
+ * @param left 左边的操作数
+ * @param right 右边的操作数
+ * @param result 用于存储计算结果
+ * @return 执行结果，成功返回 RC::SUCCESS
+ */
 RC IntegerType::multiply(const Value &left, const Value &right, Value &result) const
 {
+  // 计算两个整数的积并设置结果
   result.set_int(left.get_int() * right.get_int());
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 计算整数的负值
+ * @param val 要取负值的操作数
+ * @param result 用于存储计算结果
+ * @return 执行结果，成功返回 RC::SUCCESS
+ */
 RC IntegerType::negative(const Value &val, Value &result) const
 {
+  // 计算整数的负值并设置结果
   result.set_int(-val.get_int());
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 从字符串设置整数值
+ * @param val 要设置值的对象
+ * @param data 字符串数据
+ * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+ */
 RC IntegerType::set_value_from_str(Value &val, const string &data) const
 {
   RC                rc = RC::SUCCESS;
@@ -74,6 +139,8 @@ RC IntegerType::set_value_from_str(Value &val, const string &data) const
   deserialize_stream.str(data);
   int int_value;
   deserialize_stream >> int_value;
+  
+  // 检查解析是否成功且整个字符串都被解析
   if (!deserialize_stream || !deserialize_stream.eof()) {
     rc = RC::SCHEMA_FIELD_TYPE_MISMATCH;
   } else {
@@ -82,10 +149,16 @@ RC IntegerType::set_value_from_str(Value &val, const string &data) const
   return rc;
 }
 
+/**
+ * @brief 将整数值转换为字符串
+ * @param val 要转换的值
+ * @param result 用于存储转换结果
+ * @return 执行结果，成功返回 RC::SUCCESS
+ */
 RC IntegerType::to_string(const Value &val, string &result) const
 {
   stringstream ss;
-  ss << val.value_.int_value_;
-  result = ss.str();
+  ss << val.value_.int_value_;  // 将整数值写入字符串流
+  result = ss.str();  // 获取字符串结果
   return RC::SUCCESS;
 }
--- src/observer/common/type/integer_type.h
+++ src/observer/common/type/integer_type.h
@@ -13,25 +13,93 @@ See the Mulan PSL v2 for more details. */
 #include "common/type/data_type.h"
 
 /**
- * @brief 整型类型
+ * @brief 整型数据类型
  * @ingroup DataType
+ * @details 负责处理整数类型的所有操作，包括比较、算术运算、类型转换等
  */
 class IntegerType : public DataType
 {
 public:
+  /**
+   * @brief 构造函数
+   * @details 初始化数据类型为 INTS
+   */
   IntegerType() : DataType(AttrType::INTS) {}
+  
+  /**
+   * @brief 析构函数
+   */
   virtual ~IntegerType() {}
 
+  /**
+   * @brief 比较两个整数的大小
+   * @param left 左边的操作数
+   * @param right 右边的操作数
+   * @return 
+   *  -1 表示 left < right
+   *  0 表示 left = right
+   *  1 表示 left > right
+   *  INT32_MAX 表示未实现的比较
+   */
   int compare(const Value &left, const Value &right) const override;
 
+  /**
+   * @brief 计算 left + right，并将结果保存到 result 中
+   * @param left 左边的操作数
+   * @param right 右边的操作数
+   * @param result 用于存储计算结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC add(const Value &left, const Value &right, Value &result) const override;
+  
+  /**
+   * @brief 计算 left - right，并将结果保存到 result 中
+   * @param left 左边的操作数
+   * @param right 右边的操作数
+   * @param result 用于存储计算结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC subtract(const Value &left, const Value &right, Value &result) const override;
+  
+  /**
+   * @brief 计算 left * right，并将结果保存到 result 中
+   * @param left 左边的操作数
+   * @param right 右边的操作数
+   * @param result 用于存储计算结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC multiply(const Value &left, const Value &right, Value &result) const override;
+  
+  /**
+   * @brief 计算 -val，并将结果保存到 result 中
+   * @param val 要取负值的操作数
+   * @param result 用于存储计算结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC negative(const Value &val, Value &result) const override;
 
+  /**
+   * @brief 将整数值转换为指定类型
+   * @param val 要转换的值
+   * @param type 目标类型
+   * @param result 用于存储转换结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC cast_to(const Value &val, AttrType type, Value &result) const override;
 
+  /**
+   * @brief 从字符串设置整数值
+   * @param val 要设置值的对象
+   * @param data 字符串数据
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC set_value_from_str(Value &val, const string &data) const override;
 
+  /**
+   * @brief 将整数值转换为字符串
+   * @param val 要转换的值
+   * @param result 用于存储转换结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC to_string(const Value &val, string &result) const override;
 };

57a3d0736e232ae8c2b5ad55fb03e58826ce65fa
xinhai12310 <18135082@qq.com>
2025-10-14 05:01:47 -0700

    字符串类型

--- src/observer/common/type/char_type.cpp
+++ src/observer/common/type/char_type.cpp
@@ -13,6 +13,14 @@ See the Mulan PSL v2 for more details. */
 #include "common/type/char_type.h"
 #include "common/value.h"
 
+/**
+ * @brief 比较两个字符串值的大小
+ * @param[in] left 左操作数
+ * @param[in] right 右操作数
+ * @return 比较结果，小于0表示left小于right，等于0表示相等，大于0表示left大于right
+ * 
+ * 首先验证左右操作数类型是否都是CHARS，然后调用common::compare_string函数进行字符串比较。
+ */
 int CharType::compare(const Value &left, const Value &right) const
 {
   ASSERT(left.attr_type() == AttrType::CHARS && right.attr_type() == AttrType::CHARS, "invalid type");
@@ -20,12 +28,29 @@ int CharType::compare(const Value &left, const Value &right) const
       (void *)left.value_.pointer_value_, left.length_, (void *)right.value_.pointer_value_, right.length_);
 }
 
+/**
+ * @brief 从字符串设置值
+ * @param[in,out] val 要设置的Value对象
+ * @param[in] data 字符串数据
+ * @return 操作结果，成功返回RC::SUCCESS
+ * 
+ * 调用val的set_string方法设置字符串值。
+ */
 RC CharType::set_value_from_str(Value &val, const string &data) const
 {
   val.set_string(data.c_str());
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 将字符串值转换为指定类型
+ * @param[in] val 源值
+ * @param[in] type 目标类型
+ * @param[out] result 转换后的结果
+ * @return 操作结果，当前实现中除默认情况外都返回RC::UNIMPLEMENTED
+ * 
+ * 注意：当前实现中未完成任何具体的类型转换功能，仅返回UNIMPLEMENTED。
+ */
 RC CharType::cast_to(const Value &val, AttrType type, Value &result) const
 {
   switch (type) {
@@ -34,6 +59,13 @@ RC CharType::cast_to(const Value &val, AttrType type, Value &result) const
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 计算将字符串类型转换为指定类型的成本
+ * @param[in] type 目标类型
+ * @return 转换成本，数值越小成本越低
+ * 
+ * 当目标类型是CHARS时，成本为0；否则成本为INT32_MAX（表示转换成本极高或不支持）。
+ */
 int CharType::cast_cost(AttrType type)
 {
   if (type == AttrType::CHARS) {
@@ -42,6 +74,14 @@ int CharType::cast_cost(AttrType type)
   return INT32_MAX;
 }
 
+/**
+ * @brief 将值转换为字符串表示
+ * @param[in] val 源值
+ * @param[out] result 转换后的字符串
+ * @return 操作结果，成功返回RC::SUCCESS
+ * 
+ * 使用stringstream将字符串值转换为string对象。
+ */
 RC CharType::to_string(const Value &val, string &result) const
 {
   stringstream ss;
--- src/observer/common/type/char_type.h
+++ src/observer/common/type/char_type.h
@@ -16,21 +16,60 @@ See the Mulan PSL v2 for more details. */
 /**
  * @brief 固定长度的字符串类型
  * @ingroup DataType
+ * 
+ * CharType类是DataType的子类，专门用于处理字符串类型的数据。
+ * 实现了字符串的比较、转换、设置值等操作。
  */
 class CharType : public DataType
 {
 public:
+  /**
+   * @brief 构造函数，初始化属性类型为CHARS
+   */
   CharType() : DataType(AttrType::CHARS) {}
 
+  /**
+   * @brief 析构函数
+   */
   virtual ~CharType() = default;
 
+  /**
+   * @brief 比较两个字符串值的大小
+   * @param[in] left 左操作数
+   * @param[in] right 右操作数
+   * @return 比较结果，小于0表示left小于right，等于0表示相等，大于0表示left大于right
+   */
   int compare(const Value &left, const Value &right) const override;
 
+  /**
+   * @brief 将字符串值转换为指定类型
+   * @param[in] val 源值
+   * @param[in] type 目标类型
+   * @param[out] result 转换后的结果
+   * @return 操作结果，成功返回RC::SUCCESS，否则返回错误码
+   */
   RC cast_to(const Value &val, AttrType type, Value &result) const override;
 
+  /**
+   * @brief 从字符串设置值
+   * @param[in,out] val 要设置的Value对象
+   * @param[in] data 字符串数据
+   * @return 操作结果，成功返回RC::SUCCESS
+   */
   RC set_value_from_str(Value &val, const string &data) const override;
 
+  /**
+   * @brief 计算将字符串类型转换为指定类型的成本
+   * @param[in] type 目标类型
+   * @return 转换成本，数值越小成本越低
+   */
   int cast_cost(AttrType type) override;
 
+  /**
+   * @brief 将值转换为字符串表示
+   * @param[in] val 源值
+   * @param[out] result 转换后的字符串
+   * @return 操作结果，成功返回RC::SUCCESS
+   */
   RC to_string(const Value &val, string &result) const override;
 };

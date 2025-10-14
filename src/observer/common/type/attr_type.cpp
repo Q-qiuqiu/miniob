735f9d0b895186b97ff40c1da61f3d8912627f96
xinhai12310 <18135082@qq.com>
2025-10-14 05:02:05 -0700

    所有属性枚举类

--- src/observer/common/type/attr_type.cpp
+++ src/observer/common/type/attr_type.cpp
@@ -12,22 +12,45 @@ See the Mulan PSL v2 for more details. */
 #include "common/lang/string.h"
 #include "common/type/attr_type.h"
 
+/**
+ * @brief 属性类型名称数组
+ * @details 存储了 AttrType 枚举中各类型对应的字符串表示，索引与枚举值一一对应
+ */
 const char *ATTR_TYPE_NAME[] = {"undefined", "chars", "ints", "floats", "vectors", "booleans"};
 
+/**
+ * @brief 将属性类型转换为字符串表示
+ * @param type 要转换的属性类型枚举值
+ * @return 对应属性类型的字符串表示，如果类型无效则返回"unknown"
+ * @note 函数会检查类型是否在有效范围内（UNDEFINED 到 MAXTYPE 之间）
+ */
 const char *attr_type_to_string(AttrType type)
 {
+  // 检查类型是否在有效范围内
   if (type >= AttrType::UNDEFINED && type < AttrType::MAXTYPE) {
+    // 将枚举值转换为整数索引，获取对应的字符串
     return ATTR_TYPE_NAME[static_cast<int>(type)];
   }
+  // 类型无效时返回"unknown"
   return "unknown";
 }
 
+/**
+ * @brief 从字符串解析属性类型
+ * @param s 表示属性类型的字符串
+ * @return 对应的属性类型枚举值，如果无法解析则返回 UNDEFINED
+ * @note 此函数使用不区分大小写的比较方式查找匹配的类型
+ */
 AttrType attr_type_from_string(const char *s)
 {
+  // 遍历所有已知的类型名称
   for (unsigned int i = 0; i < sizeof(ATTR_TYPE_NAME) / sizeof(ATTR_TYPE_NAME[0]); i++) {
+    // 不区分大小写比较字符串
     if (0 == strcasecmp(ATTR_TYPE_NAME[i], s)) {
+      // 找到匹配项，将索引转换为枚举值返回
       return (AttrType)i;
     }
   }
+  // 没有找到匹配项，返回 UNDEFINED
   return AttrType::UNDEFINED;
 }
--- src/observer/common/type/attr_type.h
+++ src/observer/common/type/attr_type.h
@@ -11,19 +11,37 @@ See the Mulan PSL v2 for more details. */
 #pragma once
 
 /**
- * @brief 属性的类型
- * @details AttrType 枚举列出了属性的各种数据类型。
+ * @brief 属性的类型定义
+ * @file attr_type.h
+ * @details 该文件定义了数据库中支持的各种数据类型，用于表的属性定义、数据存储和操作。
+ */
+
+/**
+ * @brief 属性的类型枚举
+ * @details AttrType 枚举列出了数据库支持的各种数据类型，包括字符串、整数、浮点数等。
  */
 enum class AttrType
 {
-  UNDEFINED,
-  CHARS,     ///< 字符串类型
-  INTS,      ///< 整数类型(4字节)
-  FLOATS,    ///< 浮点数类型(4字节)
-  VECTORS,   ///< 向量类型
-  BOOLEANS,  ///< boolean类型，当前不是由parser解析出来的，是程序内部使用的
-  MAXTYPE,   ///< 请在 UNDEFINED 与 MAXTYPE 之间增加新类型
+  UNDEFINED,  ///< 未定义类型
+  CHARS,      ///< 字符串类型，可变长度的字符序列
+  INTS,       ///< 整数类型，4字节有符号整数
+  FLOATS,     ///< 浮点数类型，4字节单精度浮点数
+  VECTORS,    ///< 向量类型，用于存储多维数据
+  BOOLEANS,   ///< 布尔类型，程序内部使用，不直接由解析器生成
+  MAXTYPE,    ///< 类型边界标记，请在 UNDEFINED 与 MAXTYPE 之间增加新类型
 };
 
+/**
+ * @brief 将属性类型转换为字符串表示
+ * @param type 要转换的属性类型
+ * @return 对应属性类型的字符串表示，如果类型无效则返回"unknown"
+ */
 const char *attr_type_to_string(AttrType type);
+
+/**
+ * @brief 从字符串解析属性类型
+ * @param s 表示属性类型的字符串
+ * @return 对应的属性类型枚举值，如果无法解析则返回 UNDEFINED
+ * @note 此函数不区分大小写进行比较
+ */
 AttrType    attr_type_from_string(const char *s);

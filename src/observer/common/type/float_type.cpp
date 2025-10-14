113b23beaaf41c94ae50607e46653247c4ff2789
xinhai12310 <18135082@qq.com>
2025-10-14 05:01:39 -0700

    浮点类型。

--- src/observer/common/type/float_type.cpp
+++ src/observer/common/type/float_type.cpp
@@ -16,49 +16,113 @@ See the Mulan PSL v2 for more details. */
 #include "common/lang/limits.h"
 #include "common/value.h"
 
+/**
+ * @brief 比较两个浮点数的大小
+ * @param left 左边的操作数，必须是 FLOAT 类型
+ * @param right 右边的操作数，必须是 INT 或 FLOAT 类型
+ * @return 
+ *  -1 表示 left < right
+ *  0 表示 left = right
+ *  1 表示 left > right
+ *  INT32_MAX 表示未实现的比较
+ */
 int FloatType::compare(const Value &left, const Value &right) const
 {
+  // 断言检查左操作数是否为浮点类型
   ASSERT(left.attr_type() == AttrType::FLOATS, "left type is not integer");
+  // 断言检查右操作数是否为数值类型（整数或浮点数）
   ASSERT(right.attr_type() == AttrType::INTS || right.attr_type() == AttrType::FLOATS, "right type is not numeric");
-  float left_val  = left.get_float();
-  float right_val = right.get_float();
+  
+  float left_val  = left.get_float();  // 获取左操作数的浮点值
+  float right_val = right.get_float(); // 获取右操作数的浮点值
+  
+  // 使用通用的浮点比较函数进行比较
   return common::compare_float((void *)&left_val, (void *)&right_val);
 }
 
+/**
+ * @brief 计算 left + right，并将结果保存到 result 中
+ * @param left 左边的操作数
+ * @param right 右边的操作数
+ * @param result 用于存储计算结果
+ * @return 执行结果，成功返回 RC::SUCCESS
+ */
 RC FloatType::add(const Value &left, const Value &right, Value &result) const
 {
+  // 计算两个浮点数的和并设置结果
   result.set_float(left.get_float() + right.get_float());
   return RC::SUCCESS;
 }
+
+/**
+ * @brief 计算 left - right，并将结果保存到 result 中
+ * @param left 左边的操作数
+ * @param right 右边的操作数
+ * @param result 用于存储计算结果
+ * @return 执行结果，成功返回 RC::SUCCESS
+ */
 RC FloatType::subtract(const Value &left, const Value &right, Value &result) const
 {
+  // 计算两个浮点数的差并设置结果
   result.set_float(left.get_float() - right.get_float());
   return RC::SUCCESS;
 }
+
+/**
+ * @brief 计算 left * right，并将结果保存到 result 中
+ * @param left 左边的操作数
+ * @param right 右边的操作数
+ * @param result 用于存储计算结果
+ * @return 执行结果，成功返回 RC::SUCCESS
+ */
 RC FloatType::multiply(const Value &left, const Value &right, Value &result) const
 {
+  // 计算两个浮点数的积并设置结果
   result.set_float(left.get_float() * right.get_float());
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 计算 left / right，并将结果保存到 result 中
+ * @param left 左边的操作数
+ * @param right 右边的操作数
+ * @param result 用于存储计算结果
+ * @return 执行结果，成功返回 RC::SUCCESS
+ * @note 当右操作数为0时，结果设置为浮点数最大值（当前miniob没有NULL概念）
+ */
 RC FloatType::divide(const Value &left, const Value &right, Value &result) const
 {
+  // 检查右操作数是否接近于0（考虑浮点精度问题）
   if (right.get_float() > -EPSILON && right.get_float() < EPSILON) {
     // NOTE:
     // 设置为浮点数最大值是不正确的。通常的做法是设置为NULL，但是当前的miniob没有NULL概念，所以这里设置为浮点数最大值。
     result.set_float(numeric_limits<float>::max());
   } else {
+    // 计算两个浮点数的商并设置结果
     result.set_float(left.get_float() / right.get_float());
   }
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 计算 -val，并将结果保存到 result 中
+ * @param val 要取负值的操作数
+ * @param result 用于存储计算结果
+ * @return 执行结果，成功返回 RC::SUCCESS
+ */
 RC FloatType::negative(const Value &val, Value &result) const
 {
+  // 计算浮点数的负值并设置结果
   result.set_float(-val.get_float());
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 从字符串设置浮点数值
+ * @param val 要设置值的对象
+ * @param data 字符串数据
+ * @return 执行结果，成功返回 RC::SUCCESS，类型不匹配返回 RC::SCHEMA_FIELD_TYPE_MISMATCH
+ */
 RC FloatType::set_value_from_str(Value &val, const string &data) const
 {
   RC                rc = RC::SUCCESS;
@@ -67,18 +131,27 @@ RC FloatType::set_value_from_str(Value &val, const string &data) const
   deserialize_stream.str(data);
 
   float float_value;
+  // 尝试从字符串流中读取浮点数
   deserialize_stream >> float_value;
+  // 检查读取是否成功，并且流是否已经到达末尾
   if (!deserialize_stream || !deserialize_stream.eof()) {
-    rc = RC::SCHEMA_FIELD_TYPE_MISMATCH;
+    rc = RC::SCHEMA_FIELD_TYPE_MISMATCH; // 类型不匹配
   } else {
-    val.set_float(float_value);
+    val.set_float(float_value); // 设置浮点数的值
   }
   return rc;
 }
 
+/**
+ * @brief 将浮点数值转换为字符串
+ * @param val 要转换的值
+ * @param result 用于存储转换结果
+ * @return 执行结果，成功返回 RC::SUCCESS
+ */
 RC FloatType::to_string(const Value &val, string &result) const
 {
   stringstream ss;
+  // 使用通用的浮点转字符串函数将浮点数转换为字符串
   ss << common::double_to_str(val.value_.float_value_);
   result = ss.str();
   return RC::SUCCESS;
--- src/observer/common/type/float_type.h
+++ src/observer/common/type/float_type.h
@@ -15,22 +15,91 @@ See the Mulan PSL v2 for more details. */
 /**
  * @brief 浮点型数据类型
  * @ingroup DataType
+ * @details 负责处理浮点数类型的所有操作，包括比较、算术运算、类型转换等
  */
 class FloatType : public DataType
 {
 public:
+  /**
+   * @brief 构造函数
+   * @details 初始化数据类型为 FLOATS
+   */
   FloatType() : DataType(AttrType::FLOATS) {}
+  
+  /**
+   * @brief 析构函数
+   */
   virtual ~FloatType() = default;
 
+  /**
+   * @brief 比较两个浮点数的大小
+   * @param left 左边的操作数
+   * @param right 右边的操作数
+   * @return 
+   *  -1 表示 left < right
+   *  0 表示 left = right
+   *  1 表示 left > right
+   *  INT32_MAX 表示未实现的比较
+   */
   int compare(const Value &left, const Value &right) const override;
 
+  /**
+   * @brief 计算 left + right，并将结果保存到 result 中
+   * @param left 左边的操作数
+   * @param right 右边的操作数
+   * @param result 用于存储计算结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC add(const Value &left, const Value &right, Value &result) const override;
+  
+  /**
+   * @brief 计算 left - right，并将结果保存到 result 中
+   * @param left 左边的操作数
+   * @param right 右边的操作数
+   * @param result 用于存储计算结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC subtract(const Value &left, const Value &right, Value &result) const override;
+  
+  /**
+   * @brief 计算 left * right，并将结果保存到 result 中
+   * @param left 左边的操作数
+   * @param right 右边的操作数
+   * @param result 用于存储计算结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC multiply(const Value &left, const Value &right, Value &result) const override;
+  
+  /**
+   * @brief 计算 left / right，并将结果保存到 result 中
+   * @param left 左边的操作数
+   * @param right 右边的操作数
+   * @param result 用于存储计算结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC divide(const Value &left, const Value &right, Value &result) const override;
+  
+  /**
+   * @brief 计算 -val，并将结果保存到 result 中
+   * @param val 要取负值的操作数
+   * @param result 用于存储计算结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC negative(const Value &val, Value &result) const override;
 
+  /**
+   * @brief 从字符串设置浮点数值
+   * @param val 要设置值的对象
+   * @param data 字符串数据
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC set_value_from_str(Value &val, const string &data) const override;
 
+  /**
+   * @brief 将浮点数值转换为字符串
+   * @param val 要转换的值
+   * @param result 用于存储转换结果
+   * @return 执行结果，成功返回 RC::SUCCESS，否则返回错误码
+   */
   RC to_string(const Value &val, string &result) const override;
 };

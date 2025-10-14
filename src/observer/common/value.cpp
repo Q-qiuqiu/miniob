30773f81ef062decec3d29347c9c7cf62d43e6d4
xinhai12310 <18135082@qq.com>
2025-10-14 05:02:47 -0700

    存储数据的类，后续使用Value进行操作，而不是使用特定的type类。

--- src/observer/common/value.cpp
+++ src/observer/common/value.cpp
@@ -20,81 +20,130 @@ See the Mulan PSL v2 for more details. */
 #include "common/lang/string.h"
 #include "common/log/log.h"
 
+/**
+ * @brief 从整数创建Value对象的构造函数实现
+ * @param[in] val 整数值
+ */
 Value::Value(int val) { set_int(val); }
 
+/**
+ * @brief 从浮点数创建Value对象的构造函数实现
+ * @param[in] val 浮点数值
+ */
 Value::Value(float val) { set_float(val); }
 
+/**
+ * @brief 从布尔值创建Value对象的构造函数实现
+ * @param[in] val 布尔值
+ */
 Value::Value(bool val) { set_boolean(val); }
 
+/**
+ * @brief 从字符串创建Value对象的构造函数实现
+ * @param[in] s 字符串指针
+ * @param[in] len 字符串长度，默认为0
+ */
 Value::Value(const char *s, int len /*= 0*/) { set_string(s, len); }
 
+/**
+ * @brief 拷贝构造函数实现
+ * @param[in] other 要拷贝的Value对象
+ * 
+ * 拷贝other的所有属性，并根据类型进行适当的数据拷贝。
+ * 对于CHARS类型，会调用set_string_from_other进行深拷贝。
+ */
 Value::Value(const Value &other)
 {
   this->attr_type_ = other.attr_type_;
   this->length_    = other.length_;
   this->own_data_  = other.own_data_;
   switch (this->attr_type_) {
-    case AttrType::CHARS: {
+    case AttrType::CHARS: {  // 对于字符串类型，需要进行深拷贝
       set_string_from_other(other);
     } break;
 
-    default: {
+    default: {  // 对于其他类型，可以直接拷贝值
       this->value_ = other.value_;
     } break;
   }
 }
 
+/**
+ * @brief 移动构造函数实现
+ * @param[in] other 要移动的Value对象
+ * 
+ * 移动other的所有资源到当前对象，并将other重置为安全状态。
+ */
 Value::Value(Value &&other)
 {
   this->attr_type_ = other.attr_type_;
   this->length_    = other.length_;
   this->own_data_  = other.own_data_;
   this->value_     = other.value_;
-  other.own_data_  = false;
+  other.own_data_  = false;  // 防止other析构时释放资源
   other.length_    = 0;
 }
 
+/**
+ * @brief 拷贝赋值运算符实现
+ * @param[in] other 要拷贝的Value对象
+ * @return 当前Value对象的引用
+ * 
+ * 先重置当前对象，然后拷贝other的所有属性和数据。
+ */
 Value &Value::operator=(const Value &other)
 {
-  if (this == &other) {
+  if (this == &other) {  // 自赋值检查
     return *this;
   }
-  reset();
+  reset();  // 先释放当前资源
   this->attr_type_ = other.attr_type_;
   this->length_    = other.length_;
   this->own_data_  = other.own_data_;
   switch (this->attr_type_) {
-    case AttrType::CHARS: {
+    case AttrType::CHARS: {  // 对于字符串类型，需要进行深拷贝
       set_string_from_other(other);
     } break;
 
-    default: {
+    default: {  // 对于其他类型，可以直接拷贝值
       this->value_ = other.value_;
     } break;
   }
   return *this;
 }
 
+/**
+ * @brief 移动赋值运算符实现
+ * @param[in] other 要移动的Value对象
+ * @return 当前Value对象的引用
+ * 
+ * 先重置当前对象，然后移动other的资源到当前对象。
+ */
 Value &Value::operator=(Value &&other)
 {
-  if (this == &other) {
+  if (this == &other) {  // 自赋值检查
     return *this;
   }
-  reset();
+  reset();  // 先释放当前资源
   this->attr_type_ = other.attr_type_;
   this->length_    = other.length_;
   this->own_data_  = other.own_data_;
   this->value_     = other.value_;
-  other.own_data_  = false;
+  other.own_data_  = false;  // 防止other析构时释放资源
   other.length_    = 0;
   return *this;
 }
 
+/**
+ * @brief 重置Value对象实现
+ * 
+ * 释放可能占用的内存资源，并将所有成员变量重置为初始状态。
+ */
 void Value::reset()
 {
   switch (attr_type_) {
     case AttrType::CHARS:
-      if (own_data_ && value_.pointer_value_ != nullptr) {
+      if (own_data_ && value_.pointer_value_ != nullptr) {  // 如果是自己分配的字符串内存，则释放
         delete[] value_.pointer_value_;
         value_.pointer_value_ = nullptr;
       }
@@ -107,30 +156,43 @@ void Value::reset()
   own_data_  = false;
 }
 
+/**
+ * @brief 设置值的数据实现
+ * @param[in] data 数据指针
+ * @param[in] length 数据长度
+ * 
+ * 根据当前的属性类型，将data中的数据存储到value_的相应成员中。
+ */
 void Value::set_data(char *data, int length)
 {
   switch (attr_type_) {
-    case AttrType::CHARS: {
+    case AttrType::CHARS: {  // 字符串类型
       set_string(data, length);
     } break;
-    case AttrType::INTS: {
+    case AttrType::INTS: {  // 整数类型
       value_.int_value_ = *(int *)data;
       length_           = length;
     } break;
-    case AttrType::FLOATS: {
+    case AttrType::FLOATS: {  // 浮点数类型
       value_.float_value_ = *(float *)data;
       length_             = length;
     } break;
-    case AttrType::BOOLEANS: {
+    case AttrType::BOOLEANS: {  // 布尔类型
       value_.bool_value_ = *(int *)data != 0;
       length_            = length;
     } break;
-    default: {
+    default: {  // 未知类型
       LOG_WARN("unknown data type: %d", attr_type_);
     } break;
   }
 }
 
+/**
+ * @brief 设置整数值实现
+ * @param[in] val 整数值
+ * 
+ * 先重置对象，然后设置类型为INTS，并存储整数值。
+ */
 void Value::set_int(int val)
 {
   reset();
@@ -139,6 +201,12 @@ void Value::set_int(int val)
   length_           = sizeof(val);
 }
 
+/**
+ * @brief 设置浮点数值实现
+ * @param[in] val 浮点数值
+ * 
+ * 先重置对象，然后设置类型为FLOATS，并存储浮点数值。
+ */
 void Value::set_float(float val)
 {
   reset();
@@ -146,6 +214,13 @@ void Value::set_float(float val)
   value_.float_value_ = val;
   length_             = sizeof(val);
 }
+
+/**
+ * @brief 设置布尔值实现
+ * @param[in] val 布尔值
+ * 
+ * 先重置对象，然后设置类型为BOOLEANS，并存储布尔值。
+ */
 void Value::set_boolean(bool val)
 {
   reset();
@@ -154,27 +229,41 @@ void Value::set_boolean(bool val)
   length_            = sizeof(val);
 }
 
+/**
+ * @brief 设置字符串值实现
+ * @param[in] s 字符串指针
+ * @param[in] len 字符串长度，默认为0
+ * 
+ * 先重置对象，然后设置类型为CHARS，并根据需要分配内存存储字符串。
+ * 如果len为0，则自动计算字符串长度。
+ */
 void Value::set_string(const char *s, int len /*= 0*/)
 {
   reset();
   attr_type_ = AttrType::CHARS;
-  if (s == nullptr) {
+  if (s == nullptr) {  // 空字符串处理
     value_.pointer_value_ = nullptr;
     length_               = 0;
   } else {
-    own_data_ = true;
+    own_data_ = true;  // 标记为自己分配内存
     if (len > 0) {
-      len = strnlen(s, len);
+      len = strnlen(s, len);  // 计算指定长度内的实际字符串长度
     } else {
-      len = strlen(s);
+      len = strlen(s);  // 自动计算字符串长度
     }
-    value_.pointer_value_ = new char[len + 1];
+    value_.pointer_value_ = new char[len + 1];  // 分配内存，+1 用于存储结束符
     length_               = len;
-    memcpy(value_.pointer_value_, s, len);
-    value_.pointer_value_[len] = '\0';
+    memcpy(value_.pointer_value_, s, len);  // 复制字符串内容
+    value_.pointer_value_[len] = '\0';  // 添加结束符
   }
 }
 
+/**
+ * @brief 从另一个Value对象设置值实现
+ * @param[in] value 源Value对象
+ * 
+ * 根据value的类型，调用相应的set_xxx方法设置值。
+ */
 void Value::set_value(const Value &value)
 {
   switch (value.attr_type_) {
@@ -196,6 +285,12 @@ void Value::set_value(const Value &value)
   }
 }
 
+/**
+ * @brief 从另一个Value对象设置字符串值实现
+ * @param[in] other 源Value对象
+ * 
+ * 假设当前对象的类型已经是CHARS，从other复制字符串内容。
+ */
 void Value::set_string_from_other(const Value &other)
 {
   ASSERT(attr_type_ == AttrType::CHARS, "attr type is not CHARS");
@@ -206,18 +301,31 @@ void Value::set_string_from_other(const Value &other)
   }
 }
 
+/**
+ * @brief 获取值的原始数据指针实现
+ * @return 数据指针
+ * 
+ * 根据当前的属性类型，返回相应的数据指针。
+ */
 const char *Value::data() const
 {
   switch (attr_type_) {
-    case AttrType::CHARS: {
+    case AttrType::CHARS: {  // 字符串类型，直接返回指针
       return value_.pointer_value_;
     } break;
-    default: {
+    default: {  // 其他类型，返回value_的地址
       return (const char *)&value_;
     } break;
   }
 }
 
+/**
+ * @brief 将值转换为字符串表示实现
+ * @return 字符串表示形式
+ * 
+ * 调用相应的DataType实例的to_string方法进行转换。
+ * 如果转换失败，记录警告日志并返回空字符串。
+ */
 string Value::to_string() const
 {
   string res;
@@ -229,12 +337,26 @@ string Value::to_string() const
   return res;
 }
 
+/**
+ * @brief 比较两个值的大小实现
+ * @param[in] other 要比较的另一个值
+ * @return 比较结果，小于0表示当前值小于other，等于0表示相等，大于0表示当前值大于other
+ * 
+ * 调用相应的DataType实例的compare方法进行比较。
+ */
 int Value::compare(const Value &other) const { return DataType::type_instance(this->attr_type_)->compare(*this, other); }
 
+/**
+ * @brief 获取整数值实现
+ * @return 转换后的整数值
+ * 
+ * 根据当前的属性类型，将值转换为整数返回。
+ * 对于无法转换的情况，返回0并记录日志。
+ */
 int Value::get_int() const
 {
   switch (attr_type_) {
-    case AttrType::CHARS: {
+    case AttrType::CHARS: {  // 字符串转换为整数
       try {
         return (int)(stol(value_.pointer_value_));
       } catch (exception const &ex) {
@@ -242,16 +364,16 @@ int Value::get_int() const
         return 0;
       }
     }
-    case AttrType::INTS: {
+    case AttrType::INTS: {  // 直接返回整数值
       return value_.int_value_;
     }
-    case AttrType::FLOATS: {
+    case AttrType::FLOATS: {  // 浮点数转换为整数
       return (int)(value_.float_value_);
     }
-    case AttrType::BOOLEANS: {
+    case AttrType::BOOLEANS: {  // 布尔值转换为整数
       return (int)(value_.bool_value_);
     }
-    default: {
+    default: {  // 未知类型
       LOG_WARN("unknown data type. type=%d", attr_type_);
       return 0;
     }
@@ -259,10 +381,17 @@ int Value::get_int() const
   return 0;
 }
 
+/**
+ * @brief 获取浮点数值实现
+ * @return 转换后的浮点数值
+ * 
+ * 根据当前的属性类型，将值转换为浮点数返回。
+ * 对于无法转换的情况，返回0.0并记录日志。
+ */
 float Value::get_float() const
 {
   switch (attr_type_) {
-    case AttrType::CHARS: {
+    case AttrType::CHARS: {  // 字符串转换为浮点数
       try {
         return stof(value_.pointer_value_);
       } catch (exception const &ex) {
@@ -270,16 +399,16 @@ float Value::get_float() const
         return 0.0;
       }
     } break;
-    case AttrType::INTS: {
+    case AttrType::INTS: {  // 整数转换为浮点数
       return float(value_.int_value_);
     } break;
-    case AttrType::FLOATS: {
+    case AttrType::FLOATS: {  // 直接返回浮点数值
       return value_.float_value_;
     } break;
-    case AttrType::BOOLEANS: {
+    case AttrType::BOOLEANS: {  // 布尔值转换为浮点数
       return float(value_.bool_value_);
     } break;
-    default: {
+    default: {  // 未知类型
       LOG_WARN("unknown data type. type=%d", attr_type_);
       return 0;
     }
@@ -287,40 +416,54 @@ float Value::get_float() const
   return 0;
 }
 
+/**
+ * @brief 获取字符串值实现
+ * @return 字符串表示形式
+ * 
+ * 调用to_string方法获取字符串表示。
+ */
 string Value::get_string() const { return this->to_string(); }
 
+/**
+ * @brief 获取布尔值实现
+ * @return 转换后的布尔值
+ * 
+ * 根据当前的属性类型，将值转换为布尔值返回。
+ * 对于数字类型，非零值为true，零为false。
+ * 对于字符串，尝试转换为数字判断，或者检查是否为空字符串。
+ */
 bool Value::get_boolean() const
 {
   switch (attr_type_) {
-    case AttrType::CHARS: {
+    case AttrType::CHARS: {  // 字符串转换为布尔值
       try {
         float val = stof(value_.pointer_value_);
-        if (val >= EPSILON || val <= -EPSILON) {
+        if (val >= EPSILON || val <= -EPSILON) {  // 浮点数非零
           return true;
         }
 
         int int_val = stol(value_.pointer_value_);
-        if (int_val != 0) {
+        if (int_val != 0) {  // 整数非零
           return true;
         }
 
-        return value_.pointer_value_ != nullptr;
+        return value_.pointer_value_ != nullptr;  // 非空字符串
       } catch (exception const &ex) {
         LOG_TRACE("failed to convert string to float or integer. s=%s, ex=%s", value_.pointer_value_, ex.what());
-        return value_.pointer_value_ != nullptr;
+        return value_.pointer_value_ != nullptr;  // 非空字符串
       }
     } break;
-    case AttrType::INTS: {
+    case AttrType::INTS: {  // 整数转换为布尔值
       return value_.int_value_ != 0;
     } break;
-    case AttrType::FLOATS: {
+    case AttrType::FLOATS: {  // 浮点数转换为布尔值
       float val = value_.float_value_;
       return val >= EPSILON || val <= -EPSILON;
     } break;
-    case AttrType::BOOLEANS: {
+    case AttrType::BOOLEANS: {  // 直接返回布尔值
       return value_.bool_value_;
     } break;
-    default: {
+    default: {  // 未知类型
       LOG_WARN("unknown data type. type=%d", attr_type_);
       return false;
     }
--- src/observer/common/value.h
+++ src/observer/common/value.h
@@ -22,9 +22,9 @@ See the Mulan PSL v2 for more details. */
 /**
  * @brief 属性的值
  * @ingroup DataType
- * @details 与DataType，就是数据类型，配套完成各种算术运算、比较、类型转换等操作。这里同时记录了数据的值与类型。
- * 当需要对值做运算时，建议使用类似 Value::add 的操作而不是 DataType::add。在进行运算前，应该设置好结果的类型，
- * 比如进行两个INT类型的除法运算时，结果类型应该设置为FLOAT。
+ * @details 与DataType（数据类型）配套完成各种算术运算、比较、类型转换等操作。
+ * 这里同时记录了数据的值与类型。当需要对值做运算时，建议使用类似Value::add的操作而不是DataType::add。
+ * 在进行运算前，应该设置好结果的类型，比如进行两个INT类型的除法运算时，结果类型应该设置为FLOAT。
  */
 class Value final
 {
@@ -36,68 +36,212 @@ public:
   friend class CharType;
   friend class VectorType;
 
+  /**
+   * @brief 默认构造函数，创建一个未定义类型的值
+   */
   Value() = default;
 
+  /**
+   * @brief 析构函数，释放可能占用的内存资源
+   */
   ~Value() { reset(); }
 
+  /**
+   * @brief 构造函数，从指定数据创建一个值
+   * @param[in] attr_type 属性类型
+   * @param[in] data 数据指针
+   * @param[in] length 数据长度，默认为4
+   */
   Value(AttrType attr_type, char *data, int length = 4) : attr_type_(attr_type) { this->set_data(data, length); }
 
+  /**
+   * @brief 构造函数，从整数创建一个值
+   * @param[in] val 整数值
+   */
   explicit Value(int val);
+  
+  /**
+   * @brief 构造函数，从浮点数创建一个值
+   * @param[in] val 浮点数值
+   */
   explicit Value(float val);
+  
+  /**
+   * @brief 构造函数，从布尔值创建一个值
+   * @param[in] val 布尔值
+   */
   explicit Value(bool val);
+  
+  /**
+   * @brief 构造函数，从字符串创建一个值
+   * @param[in] s 字符串指针
+   * @param[in] len 字符串长度，默认为0（表示自动计算长度）
+   */
   explicit Value(const char *s, int len = 0);
 
+  /**
+   * @brief 拷贝构造函数
+   * @param[in] other 要拷贝的Value对象
+   */
   Value(const Value &other);
+  
+  /**
+   * @brief 移动构造函数
+   * @param[in] other 要移动的Value对象
+   */
   Value(Value &&other);
 
+  /**
+   * @brief 拷贝赋值运算符
+   * @param[in] other 要拷贝的Value对象
+   * @return 当前Value对象的引用
+   */
   Value &operator=(const Value &other);
+  
+  /**
+   * @brief 移动赋值运算符
+   * @param[in] other 要移动的Value对象
+   * @return 当前Value对象的引用
+   */
   Value &operator=(Value &&other);
 
+  /**
+   * @brief 重置Value对象，释放资源并将类型设置为未定义
+   */
   void reset();
 
+  /**
+   * @brief 执行加法运算
+   * @param[in] left 左操作数
+   * @param[in] right 右操作数
+   * @param[out] result 运算结果
+   * @return 操作结果，成功返回RC::SUCCESS
+   */
   static RC add(const Value &left, const Value &right, Value &result)
   {
     return DataType::type_instance(result.attr_type())->add(left, right, result);
   }
 
+  /**
+   * @brief 执行减法运算
+   * @param[in] left 左操作数
+   * @param[in] right 右操作数
+   * @param[out] result 运算结果
+   * @return 操作结果，成功返回RC::SUCCESS
+   */
   static RC subtract(const Value &left, const Value &right, Value &result)
   {
     return DataType::type_instance(result.attr_type())->subtract(left, right, result);
   }
 
+  /**
+   * @brief 执行乘法运算
+   * @param[in] left 左操作数
+   * @param[in] right 右操作数
+   * @param[out] result 运算结果
+   * @return 操作结果，成功返回RC::SUCCESS
+   */
   static RC multiply(const Value &left, const Value &right, Value &result)
   {
     return DataType::type_instance(result.attr_type())->multiply(left, right, result);
   }
 
+  /**
+   * @brief 执行除法运算
+   * @param[in] left 左操作数
+   * @param[in] right 右操作数
+   * @param[out] result 运算结果
+   * @return 操作结果，成功返回RC::SUCCESS
+   */
   static RC divide(const Value &left, const Value &right, Value &result)
   {
     return DataType::type_instance(result.attr_type())->divide(left, right, result);
   }
 
+  /**
+   * @brief 执行取负运算
+   * @param[in] value 操作数
+   * @param[out] result 运算结果
+   * @return 操作结果，成功返回RC::SUCCESS
+   */
   static RC negative(const Value &value, Value &result)
   {
     return DataType::type_instance(result.attr_type())->negative(value, result);
   }
 
+  /**
+   * @brief 类型转换
+   * @param[in] value 源值
+   * @param[in] to_type 目标类型
+   * @param[out] result 转换后的结果
+   * @return 操作结果，成功返回RC::SUCCESS
+   */
   static RC cast_to(const Value &value, AttrType to_type, Value &result)
   {
     return DataType::type_instance(value.attr_type())->cast_to(value, to_type, result);
   }
 
+  /**
+   * @brief 设置值的类型
+   * @param[in] type 目标类型
+   */
   void set_type(AttrType type) { this->attr_type_ = type; }
+  
+  /**
+   * @brief 设置值的数据
+   * @param[in] data 数据指针
+   * @param[in] length 数据长度
+   */
   void set_data(char *data, int length);
+  
+  /**
+   * @brief 设置值的数据（常量版本）
+   * @param[in] data 常量数据指针
+   * @param[in] length 数据长度
+   */
   void set_data(const char *data, int length) { this->set_data(const_cast<char *>(data), length); }
+  
+  /**
+   * @brief 从另一个Value对象设置值
+   * @param[in] value 源Value对象
+   */
   void set_value(const Value &value);
+  
+  /**
+   * @brief 设置布尔值
+   * @param[in] val 布尔值
+   */
   void set_boolean(bool val);
 
+  /**
+   * @brief 将值转换为字符串表示
+   * @return 字符串表示形式
+   */
   string to_string() const;
 
+  /**
+   * @brief 比较两个值的大小
+   * @param[in] other 要比较的另一个值
+   * @return 比较结果，小于0表示当前值小于other，等于0表示相等，大于0表示当前值大于other
+   */
   int compare(const Value &other) const;
 
+  /**
+   * @brief 获取值的原始数据指针
+   * @return 数据指针
+   */
   const char *data() const;
 
+  /**
+   * @brief 获取值的长度
+   * @return 长度值
+   */
   int      length() const { return length_; }
+  
+  /**
+   * @brief 获取值的属性类型
+   * @return 属性类型
+   */
   AttrType attr_type() const { return attr_type_; }
 
 public:
@@ -105,27 +249,73 @@ public:
    * 获取对应的值
    * 如果当前的类型与期望获取的类型不符，就会执行转换操作
    */
+  /**
+   * @brief 获取整数值
+   * @return 转换后的整数值
+   */
   int    get_int() const;
+  
+  /**
+   * @brief 获取浮点数值
+   * @return 转换后的浮点数值
+   */
   float  get_float() const;
+  
+  /**
+   * @brief 获取字符串值
+   * @return 字符串表示形式
+   */
   string get_string() const;
+  
+  /**
+   * @brief 获取布尔值
+   * @return 转换后的布尔值
+   */
   bool   get_boolean() const;
 
 public:
+  /**
+   * @brief 设置整数值
+   * @param[in] val 整数值
+   */
   void set_int(int val);
+  
+  /**
+   * @brief 设置浮点数值
+   * @param[in] val 浮点数值
+   */
   void set_float(float val);
+  
+  /**
+   * @brief 设置字符串值
+   * @param[in] s 字符串指针
+   * @param[in] len 字符串长度，默认为0（表示自动计算长度）
+   */
   void set_string(const char *s, int len = 0);
+  
+  /**
+   * @brief 从另一个Value对象设置字符串值
+   * @param[in] other 源Value对象
+   */
   void set_string_from_other(const Value &other);
 
 private:
+  /// 属性类型，默认为UNDEFINED
   AttrType attr_type_ = AttrType::UNDEFINED;
+  
+  /// 数据长度
   int      length_    = 0;
 
+  /**
+   * @brief 值的联合体，用于存储不同类型的数据
+   * @details 根据attr_type_的值，使用不同的成员
+   */
   union Val
   {
-    int32_t int_value_;
-    float   float_value_;
-    bool    bool_value_;
-    char   *pointer_value_;
+    int32_t int_value_;     ///< 整数值
+    float   float_value_;   ///< 浮点数值
+    bool    bool_value_;    ///< 布尔值
+    char   *pointer_value_; ///< 指针值，用于字符串等
   } value_ = {.int_value_ = 0};
 
   /// 是否申请并占有内存, 目前对于 CHARS 类型 own_data_ 为true, 其余类型 own_data_ 为false

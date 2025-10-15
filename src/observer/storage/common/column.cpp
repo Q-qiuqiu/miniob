054d827126c2e2f9486858c8db5292438da000f4
xinhai12310 <18135082@qq.com>
2025-10-15 04:46:24 -0700

    列类，包含连续内存中的多个相同类型的值。

--- src/observer/storage/common/column.cpp
+++ src/observer/storage/common/column.cpp
@@ -11,6 +11,11 @@ See the Mulan PSL v2 for more details. */
 #include "common/log/log.h"
 #include "storage/common/column.h"
 
+/**
+ * @brief 使用字段元数据构造列
+ * @param[in] meta 字段元数据，包含类型和长度信息
+ * @param[in] size 初始容量大小，默认值从DEFAULT_CAPACITY获取
+ */
 Column::Column(const FieldMeta &meta, size_t size)
     : data_(nullptr),
       count_(0),
@@ -25,6 +30,12 @@ Column::Column(const FieldMeta &meta, size_t size)
   capacity_ = size;
 }
 
+/**
+ * @brief 使用属性类型和长度构造列
+ * @param[in] attr_type 属性类型
+ * @param[in] attr_len 属性长度
+ * @param[in] capacity 初始容量大小
+ */
 Column::Column(AttrType attr_type, int attr_len, size_t capacity)
 {
   attr_type_   = attr_type;
@@ -36,6 +47,12 @@ Column::Column(AttrType attr_type, int attr_len, size_t capacity)
   column_type_ = Type::NORMAL_COLUMN;
 }
 
+/**
+ * @brief 使用字段元数据初始化列
+ * @param[in] meta 字段元数据
+ * @param[in] size 初始容量大小，默认值从DEFAULT_CAPACITY获取
+ * @details 先调用reset()清空当前资源，然后根据字段元数据重新分配内存并初始化
+ */
 void Column::init(const FieldMeta &meta, size_t size)
 {
   reset();
@@ -48,6 +65,13 @@ void Column::init(const FieldMeta &meta, size_t size)
   column_type_ = Type::NORMAL_COLUMN;
 }
 
+/**
+ * @brief 使用属性类型和长度初始化列
+ * @param[in] attr_type 属性类型
+ * @param[in] attr_len 属性长度
+ * @param[in] capacity 初始容量大小，默认值从DEFAULT_CAPACITY获取
+ * @details 先调用reset()清空当前资源，然后根据属性类型和长度重新分配内存并初始化
+ */
 void Column::init(AttrType attr_type, int attr_len, size_t capacity)
 {
   reset();
@@ -60,6 +84,11 @@ void Column::init(AttrType attr_type, int attr_len, size_t capacity)
   column_type_ = Type::NORMAL_COLUMN;
 }
 
+/**
+ * @brief 使用值初始化列（创建常量列）
+ * @param[in] value 用于初始化的值
+ * @details 先调用reset()清空当前资源，然后创建一个只包含一个值的常量列
+ */
 void Column::init(const Value &value)
 {
   reset();
@@ -73,6 +102,11 @@ void Column::init(const Value &value)
   column_type_ = Type::CONSTANT_COLUMN;
 }
 
+/**
+ * @brief 重置列，释放资源并重置所有成员变量
+ * @details 如果拥有数据内存（own_为true），则释放data_指向的内存
+ *          重置所有成员变量为默认初始状态
+ */
 void Column::reset()
 {
   if (data_ != nullptr && own_) {
@@ -86,8 +120,21 @@ void Column::reset()
   attr_len_  = -1;
 }
 
+/**
+ * @brief 向列中追加一个值
+ * @param[in] data 要追加的数据指针
+ * @return 操作成功返回RC::SUCCESS，否则返回错误码
+ * @details 内部调用append方法，传入count=1
+ */
 RC Column::append_one(char *data) { return append(data, 1); }
 
+/**
+ * @brief 向列中追加多个值
+ * @param[in] data 要被写入数据的起始地址
+ * @param[in] count 要写入数据的长度（这里指列值的个数，而不是字节）
+ * @return 操作成功返回RC::SUCCESS，否则返回错误码
+ * @details 检查列是否拥有内存以及是否有足够空间，然后通过memcpy批量复制数据
+ */
 RC Column::append(char *data, int count)
 {
   if (!own_) {
@@ -106,6 +153,11 @@ RC Column::append(char *data, int count)
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 获取指定索引位置的列值
+ * @param[in] index 要获取的值的索引位置
+ * @return 索引位置的值对象，如果索引无效则返回空值
+ */
 Value Column::get_value(int index) const
 {
   if (index >= count_ || index < 0) {
@@ -114,6 +166,11 @@ Value Column::get_value(int index) const
   return Value(attr_type_, &data_[index * attr_len_], attr_len_);
 }
 
+/**
+ * @brief 引用另一个列对象
+ * @param[in] column 要引用的列对象
+ * @details 先调用reset()清空当前资源，然后共享另一个列的内存和属性信息，不拥有内存所有权
+ */
 void Column::reference(const Column &column)
 {
   if (this == &column) {
--- src/observer/storage/common/column.h
+++ src/observer/storage/common/column.h
@@ -15,52 +15,128 @@ See the Mulan PSL v2 for more details. */
 #include "storage/field/field_meta.h"
 
 /**
- * @brief A column contains multiple values in contiguous memory with a specified type.
+ * @brief Column 类更像是一个通用的列数据容器，它在数据库的查询执行过程中非常有用，
+ * 尤其是在需要批量处理列数据的场景下。
+ * 例如，在查询执行计划中，可能需要对某一列的数据进行过滤、排序、聚合等操作，
+ * 使用 Column 类可以方便地管理这些中间数据。
+
+ * 虽然列式存储的核心思想也是按列组织和存储数据，
+ * 但完整的列式存储引擎通常还需要考虑更多因素，
+ * 如压缩、编码、向量化执行等。
+ * 而当前的 Column 类实现相对简单，主要关注于内存中列数据的基本管理。
+ */
+
+/**
+ * @brief 列类，包含连续内存中的多个相同类型的值
+ * @details 用于存储表中某一列的所有值，支持普通列和常量列两种类型
+ * @note 当前仅支持定长类型
  */
 // TODO: `Column` currently only support fixed-length type.
 class Column
 {
 public:
+  /**
+   * @brief 列类型枚举
+   */
   enum class Type
   {
-    NORMAL_COLUMN,   /// Normal column represents a list of fixed-length values
-    CONSTANT_COLUMN  /// Constant column represents a single value
+    NORMAL_COLUMN,   ///< 普通列，表示一系列定长值的列表
+    CONSTANT_COLUMN  ///< 常量列，表示单个值
   };
 
+  /**
+   * @brief 默认构造函数
+   */
   Column()               = default;
+  
+  /**
+   * @brief 拷贝构造函数（禁用）
+   */
   Column(const Column &) = delete;
+  
+  /**
+   * @brief 移动构造函数（禁用）
+   */
   Column(Column &&)      = delete;
 
+  /**
+   * @brief 带字段元数据的构造函数
+   * @param[in] meta 字段元数据
+   * @param[in] size 初始容量大小
+   */
   Column(const FieldMeta &meta, size_t size = DEFAULT_CAPACITY);
+  
+  /**
+   * @brief 带属性类型和长度的构造函数
+   * @param[in] attr_type 属性类型
+   * @param[in] attr_len 属性长度
+   * @param[in] size 初始容量大小
+   */
   Column(AttrType attr_type, int attr_len, size_t size = DEFAULT_CAPACITY);
 
+  /**
+   * @brief 使用字段元数据初始化列
+   * @param[in] meta 字段元数据
+   * @param[in] size 初始容量大小
+   */
   void init(const FieldMeta &meta, size_t size = DEFAULT_CAPACITY);
+  
+  /**
+   * @brief 使用属性类型和长度初始化列
+   * @param[in] attr_type 属性类型
+   * @param[in] attr_len 属性长度
+   * @param[in] size 初始容量大小
+   */
   void init(AttrType attr_type, int attr_len, size_t size = DEFAULT_CAPACITY);
+  
+  /**
+   * @brief 使用值初始化列（常量列）
+   * @param[in] value 初始值
+   */
   void init(const Value &value);
 
+  /**
+   * @brief 析构函数
+   */
   virtual ~Column() { reset(); }
 
+  /**
+   * @brief 重置列，释放资源并重置所有成员变量
+   */
   void reset();
 
+  /**
+   * @brief 向列中追加一个值
+   * @param[in] data 要追加的数据指针
+   * @return 操作成功返回RC::SUCCESS，否则返回错误码
+   */
   RC append_one(char *data);
 
   /**
    * @brief 向 Column 追加写入数据
-   * @param data 要被写入数据的起始地址
-   * @param count 要写入数据的长度（这里指列值的个数，而不是字节）
+   * @param[in] data 要被写入数据的起始地址
+   * @param[in] count 要写入数据的长度（这里指列值的个数，而不是字节）
+   * @return 操作成功返回RC::SUCCESS，否则返回错误码
    */
   RC append(char *data, int count);
 
   /**
    * @brief 获取 index 位置的列值
+   * @param[in] index 要获取的值的索引位置
+   * @return 索引位置的值对象
    */
   Value get_value(int index) const;
 
   /**
    * @brief 获取列数据的实际大小（字节）
+   * @return 列数据的字节数
    */
   int data_len() const { return count_ * attr_len_; }
 
+  /**
+   * @brief 获取列数据的指针
+   * @return 指向列数据的指针
+   */
   char *data() const { return data_; }
 
   /**
@@ -70,32 +146,90 @@ public:
 
   /**
    * @brief 引用另一个 Column
+   * @param[in] column 要引用的列对象
    */
   void reference(const Column &column);
 
+  /**
+   * @brief 设置列类型
+   * @param[in] column_type 列类型
+   */
   void set_column_type(Type column_type) { column_type_ = column_type; }
+  
+  /**
+   * @brief 设置列值数量
+   * @param[in] count 列值数量
+   */
   void set_count(int count) { count_ = count; }
 
+  /**
+   * @brief 获取列值数量
+   * @return 列值数量
+   */
   int      count() const { return count_; }
+  
+  /**
+   * @brief 获取列容量
+   * @return 列容量
+   */
   int      capacity() const { return capacity_; }
+  
+  /**
+   * @brief 获取列属性类型
+   * @return 列属性类型
+   */
   AttrType attr_type() const { return attr_type_; }
+  
+  /**
+   * @brief 获取列属性长度
+   * @return 列属性长度
+   */
   int      attr_len() const { return attr_len_; }
+  
+  /**
+   * @brief 获取列类型
+   * @return 列类型
+   */
   Type     column_type() const { return column_type_; }
 
 private:
+  /**
+   * @brief 默认容量大小
+   */
   static constexpr size_t DEFAULT_CAPACITY = 8192;
 
+  /**
+   * @brief 列数据缓冲区指针
+   */
   char *data_ = nullptr;
-  /// 当前列值数量
+  
+  /**
+   * @brief 当前列值数量
+   */
   int count_ = 0;
-  /// 当前容量，count_ <= capacity_
+  
+  /**
+   * @brief 当前容量，count_ <= capacity_
+   */
   int capacity_ = 0;
-  /// 是否拥有内存
+  
+  /**
+   * @brief 是否拥有内存
+   */
   bool own_ = true;
-  /// 列属性类型
+  
+  /**
+   * @brief 列属性类型
+   */
   AttrType attr_type_ = AttrType::UNDEFINED;
-  /// 列属性类型长度（目前只支持定长）
+  
+  /**
+   * @brief 列属性类型长度（目前只支持定长）
+   */
   int attr_len_ = -1;
-  /// 列类型
+  
+  /**
+   * @brief 列类型
+   */
   Type column_type_ = Type::NORMAL_COLUMN;
 };

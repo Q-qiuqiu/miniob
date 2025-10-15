1e8fbe1827e4f39cb74a907dbb880b2945e1613b
xinhai12310 <18135082@qq.com>
2025-10-15 10:01:37 -0700

    索引元数据

--- src/observer/storage/index/index_meta.cpp
+++ src/observer/storage/index/index_meta.cpp
@@ -19,9 +19,23 @@ See the Mulan PSL v2 for more details. */
 #include "storage/table/table_meta.h"
 #include "json/json.h"
 
+/**
+ * @brief JSON序列化时的索引名称字段名
+ */
 const static Json::StaticString FIELD_NAME("name");
+
+/**
+ * @brief JSON序列化时的索引字段名
+ */
 const static Json::StaticString FIELD_FIELD_NAME("field_name");
 
+/**
+ * @brief 初始化索引元数据实现
+ * @param name 索引名称
+ * @param field 字段元数据对象
+ * @return 操作结果，成功返回RC::SUCCESS，失败返回相应错误码
+ * @details 验证索引名称是否有效，并设置索引名称和对应的字段名
+ */
 RC IndexMeta::init(const char *name, const FieldMeta &field)
 {
   if (common::is_blank(name)) {
@@ -34,12 +48,25 @@ RC IndexMeta::init(const char *name, const FieldMeta &field)
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 将索引元数据转换为JSON格式实现
+ * @param json_value 输出参数，用于存储转换后的JSON值
+ * @details 将索引名称和字段名存储到JSON对象中
+ */
 void IndexMeta::to_json(Json::Value &json_value) const
 {
   json_value[FIELD_NAME]       = name_;
   json_value[FIELD_FIELD_NAME] = field_;
 }
 
+/**
+ * @brief 从JSON格式解析索引元数据实现
+ * @param table 表元数据对象
+ * @param json_value 包含索引元数据的JSON值
+ * @param index 输出参数，用于存储解析后的索引元数据
+ * @return 操作结果，成功返回RC::SUCCESS，失败返回相应错误码
+ * @details 从JSON对象中提取索引名称和字段名，验证字段是否存在于表中，然后初始化索引元数据
+ */
 RC IndexMeta::from_json(const TableMeta &table, const Json::Value &json_value, IndexMeta &index)
 {
   const Json::Value &name_value  = json_value[FIELD_NAME];
@@ -64,8 +91,21 @@ RC IndexMeta::from_json(const TableMeta &table, const Json::Value &json_value, I
   return index.init(name_value.asCString(), *field);
 }
 
+/**
+ * @brief 获取索引名称实现
+ * @return 索引名称的字符串指针
+ */
 const char *IndexMeta::name() const { return name_.c_str(); }
 
+/**
+ * @brief 获取索引对应的字段名实现
+ * @return 字段名称的字符串指针
+ */
 const char *IndexMeta::field() const { return field_.c_str(); }
 
+/**
+ * @brief 打印索引信息到输出流实现
+ * @param os 输出流对象
+ * @details 将索引的名称和对应的字段名打印到指定的输出流
+ */
 void IndexMeta::desc(ostream &os) const { os << "index name=" << name_ << ", field=" << field_; }
--- src/observer/storage/index/index_meta.h
+++ src/observer/storage/index/index_meta.h
@@ -17,10 +17,23 @@ See the Mulan PSL v2 for more details. */
 #include "common/sys/rc.h"
 #include "common/lang/string.h"
 
+/**
+ * @brief 前向声明：表元数据类
+ */
 class TableMeta;
+
+/**
+ * @brief 前向声明：字段元数据类
+ */
 class FieldMeta;
 
+/**
+ * @brief 命名空间：JSON处理相关类
+ */
 namespace Json {
+/**
+ * @brief JSON值类
+ */
 class Value;
 }  // namespace Json
 
@@ -33,21 +46,55 @@ class Value;
 class IndexMeta
 {
 public:
+  /**
+   * @brief 默认构造函数
+   */
   IndexMeta() = default;
 
+  /**
+   * @brief 初始化索引元数据
+   * @param name 索引名称
+   * @param field 字段元数据对象
+   * @return 操作结果，成功返回RC::SUCCESS，失败返回相应错误码
+   */
   RC init(const char *name, const FieldMeta &field);
 
 public:
+  /**
+   * @brief 获取索引名称
+   * @return 索引名称的字符串指针
+   */
   const char *name() const;
+  
+  /**
+   * @brief 获取索引对应的字段名
+   * @return 字段名称的字符串指针
+   */
   const char *field() const;
 
+  /**
+   * @brief 打印索引信息到输出流
+   * @param os 输出流对象
+   */
   void desc(ostream &os) const;
 
 public:
+  /**
+   * @brief 将索引元数据转换为JSON格式
+   * @param json_value 输出参数，用于存储转换后的JSON值
+   */
   void      to_json(Json::Value &json_value) const;
+  
+  /**
+   * @brief 从JSON格式解析索引元数据
+   * @param table 表元数据对象
+   * @param json_value 包含索引元数据的JSON值
+   * @param index 输出参数，用于存储解析后的索引元数据
+   * @return 操作结果，成功返回RC::SUCCESS，失败返回相应错误码
+   */
   static RC from_json(const TableMeta &table, const Json::Value &json_value, IndexMeta &index);
 
 protected:
-  string name_;   // index's name
-  string field_;  // field's name
+  string name_;   // index's name 索引名称
+  string field_;  // field's name 索引对应的字段名
 };

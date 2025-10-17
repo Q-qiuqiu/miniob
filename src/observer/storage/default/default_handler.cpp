3ec0b18d448c723e368607012bb07e910021859f
xinhai12310 <18135082@qq.com>
2025-10-17 01:45:15 -0700

    默认存储处理器。

--- src/observer/storage/default/default_handler.cpp
+++ src/observer/storage/default/default_handler.cpp
@@ -12,55 +12,83 @@ See the Mulan PSL v2 for more details. */
 // Created by Meiyi & Longda on 2021/4/13.
 //
 
-#include "storage/default/default_handler.h"
+/**
+ * @file default_handler.cpp
+ * @brief 默认存储引擎处理器的实现文件
+ * @details 实现DefaultHandler类的所有成员函数，包括数据库的创建、打开、关闭等核心操作，
+ * 以及表的管理功能。作为SQL层与存储层交互的主要接口。
+ */
 
-#include "common/lang/string.h"
-#include "common/log/log.h"
-#include "common/os/path.h"
-#include "session/session.h"
-#include "storage/common/condition_filter.h"
-#include "storage/index/bplus_tree.h"
-#include "storage/record/record_manager.h"
-#include "storage/table/table.h"
-#include "storage/trx/trx.h"
+#include "storage/default/default_handler.h"         ///< 默认处理器头文件
 
-using namespace std;
+#include "common/lang/string.h"                    ///< 字符串处理函数
+#include "common/log/log.h"                       ///< 日志系统
+#include "common/os/path.h"                       ///< 路径操作函数
+#include "session/session.h"                      ///< 会话管理
+#include "storage/common/condition_filter.h"      ///< 条件过滤器
+#include "storage/index/bplus_tree.h"             ///< B+树索引实现
+#include "storage/record/record_manager.h"        ///< 记录管理器
+#include "storage/table/table.h"                  ///< 表实现
+#include "storage/trx/trx.h"                      ///< 事务定义
 
+using namespace std;  ///< 使用标准命名空间
+
+/**
+ * @brief DefaultHandler构造函数实现
+ * @details 初始化DefaultHandler对象，无特殊初始化逻辑
+ */
 DefaultHandler::DefaultHandler() {}
 
+/**
+ * @brief DefaultHandler析构函数实现
+ * @details 在对象销毁时自动调用destroy()方法释放所有资源
+ */
 DefaultHandler::~DefaultHandler() noexcept { destroy(); }
 
+/**
+ * @brief 初始化存储引擎
+ * @details 设置存储目录，创建数据库目录，初始化系统数据库
+ * @param base_dir 存储引擎的根目录
+ * @param trx_kit_name 事务模型名称
+ * @param log_handler_name 日志处理器名称
+ * @param storage_engine 存储引擎名称
+ * @return RC 操作结果状态码，成功返回SUCCESS，失败返回相应错误码
+ */
 RC DefaultHandler::init(const char *base_dir, const char *trx_kit_name, const char *log_handler_name, const char *storage_engine)
 {
   // 检查目录是否存在，或者创建
   filesystem::path db_dir(base_dir);
-  db_dir /= "db";
+  db_dir /= "db";  ///< 数据库目录路径为base_dir/db
   error_code ec;
   if (!filesystem::is_directory(db_dir) && !filesystem::create_directories(db_dir, ec)) {
     LOG_ERROR("Cannot access base dir: %s. msg=%d:%s", db_dir.c_str(), errno, strerror(errno));
     return RC::INTERNAL;
   }
 
-  base_dir_ = base_dir;
-  db_dir_   = db_dir;
-  trx_kit_name_ = trx_kit_name;
-  log_handler_name_ = log_handler_name;
-  storage_engine_ = storage_engine;
+  // 初始化成员变量
+  base_dir_ = base_dir;           ///< 设置存储引擎根目录
+  db_dir_   = db_dir;             ///< 设置数据库目录
+  trx_kit_name_ = trx_kit_name;   ///< 设置事务模型名称
+  log_handler_name_ = log_handler_name; ///< 设置日志处理器名称
+  storage_engine_ = storage_engine;     ///< 设置存储引擎名称
 
-  const char *sys_db = "sys";
+  const char *sys_db = "sys";  ///< 系统数据库名称
 
+  // 创建系统数据库
   RC ret = create_db(sys_db);
-  if (ret != RC::SUCCESS && ret != RC::SCHEMA_DB_EXIST) {
+  if (ret != RC::SUCCESS && ret != RC::SCHEMA_DB_EXIST) {  ///< 数据库已存在是可接受的
     LOG_ERROR("Failed to create system db");
     return ret;
   }
 
+  // 打开系统数据库
   ret = open_db(sys_db);
   if (ret != RC::SUCCESS) {
     LOG_ERROR("Failed to open system db. rc=%s", strrc(ret));
     return ret;
   }
 
+  // 设置默认会话的当前数据库
   Session &default_session = Session::default_session();
   default_session.set_current_db(sys_db);
 
@@ -68,30 +96,43 @@ RC DefaultHandler::init(const char *base_dir, const char *trx_kit_name, const ch
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 销毁存储引擎
+ * @details 先同步所有数据到磁盘，然后释放所有打开的数据库资源
+ */
 void DefaultHandler::destroy()
 {
-  sync();
+  sync();  ///< 同步所有数据到磁盘
 
+  // 释放所有打开的数据库
   for (const auto &iter : opened_dbs_) {
-    delete iter.second;
+    delete iter.second;  ///< 释放数据库对象内存
   }
-  opened_dbs_.clear();
+  opened_dbs_.clear();  ///< 清空打开的数据库集合
 }
 
+/**
+ * @brief 创建数据库
+ * @details 在数据库目录下创建指定名称的数据库文件夹
+ * @param dbname 数据库名称
+ * @return RC 操作结果状态码，成功返回SUCCESS，参数错误返回INVALID_ARGUMENT，数据库已存在返回SCHEMA_DB_EXIST
+ */
 RC DefaultHandler::create_db(const char *dbname)
 {
+  // 参数有效性检查
   if (nullptr == dbname || common::is_blank(dbname)) {
     LOG_WARN("Invalid db name");
     return RC::INVALID_ARGUMENT;
   }
 
-  // 如果对应名录已经存在，返回错误
-  filesystem::path dbpath = db_dir_ / dbname;
+  // 检查数据库是否已存在
+  filesystem::path dbpath = db_dir_ / dbname;  ///< 构造数据库路径
   if (filesystem::is_directory(dbpath)) {
     LOG_WARN("Db already exists: %s", dbname);
     return RC::SCHEMA_DB_EXIST;
   }
 
+  // 创建数据库目录
   error_code ec;
   if (!filesystem::create_directories(dbpath, ec)) {
     LOG_ERROR("Create db fail: %s. error=%s", dbpath.c_str(), strerror(errno));
@@ -100,50 +141,95 @@ RC DefaultHandler::create_db(const char *dbname)
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 删除数据库
+ * @details 当前未实现此功能，直接返回INTERNAL错误码
+ * @param dbname 数据库名称
+ * @return RC 始终返回INTERNAL错误码
+ */
 RC DefaultHandler::drop_db(const char *dbname) { return RC::INTERNAL; }
 
+/**
+ * @brief 打开数据库
+ * @details 加载数据库信息到内存，初始化数据库组件
+ * @param dbname 数据库名称
+ * @return RC 操作结果状态码，成功返回SUCCESS，参数错误返回INVALID_ARGUMENT，数据库不存在返回SCHEMA_DB_NOT_EXIST
+ */
 RC DefaultHandler::open_db(const char *dbname)
 {
+  // 参数有效性检查
   if (nullptr == dbname || common::is_blank(dbname)) {
     LOG_WARN("Invalid db name");
     return RC::INVALID_ARGUMENT;
   }
 
+  // 检查数据库是否已经打开
   if (opened_dbs_.find(dbname) != opened_dbs_.end()) {
-    return RC::SUCCESS;
+    return RC::SUCCESS;  ///< 数据库已打开，直接返回成功
   }
 
+  // 检查数据库目录是否存在
   filesystem::path dbpath = db_dir_ / dbname;
   if (!filesystem::is_directory(dbpath)) {
     return RC::SCHEMA_DB_NOT_EXIST;
   }
 
-  // open db
+  // 打开数据库
   Db *db  = new Db();
   RC  ret = RC::SUCCESS;
   if ((ret = db->init(dbname, dbpath.c_str(), trx_kit_name_.c_str(), log_handler_name_.c_str(), storage_engine_.c_str())) != RC::SUCCESS) {
     LOG_ERROR("Failed to open db: %s. error=%s", dbname, strrc(ret));
-    delete db;
+    delete db;  ///< 初始化失败，释放内存
   } else {
-    opened_dbs_[dbname] = db;
+    opened_dbs_[dbname] = db;  ///< 将数据库添加到已打开数据库集合
   }
   return ret;
 }
 
+/**
+ * @brief 关闭指定数据库
+ * @details 当前未实现此功能，直接返回UNIMPLEMENTED错误码
+ * @param dbname 数据库名称
+ * @return RC 始终返回UNIMPLEMENTED错误码
+ */
 RC DefaultHandler::close_db(const char *dbname) { return RC::UNIMPLEMENTED; }
 
+/**
+ * @brief 在指定数据库下创建表
+ * @details 在已打开的数据库中创建新表
+ * @param dbname 数据库名称
+ * @param relation_name 表名
+ * @param attributes 属性信息数组，定义表的结构
+ * @return RC 操作结果状态码，成功返回SUCCESS，数据库未打开返回SCHEMA_DB_NOT_OPENED
+ * @note 代码中有TODO注释，提示未来可能移除DefaultHandler
+ */
 // TODO: remove DefaultHandler
 RC DefaultHandler::create_table(const char *dbname, const char *relation_name, span<const AttrInfoSqlNode> attributes)
 {
+  // 查找已打开的数据库
   Db *db = find_db(dbname);
   if (db == nullptr) {
     return RC::SCHEMA_DB_NOT_OPENED;
   }
+  // 委托数据库对象创建表，第三个参数为空向量（不创建索引）
   return db->create_table(relation_name, attributes, {});
 }
 
+/**
+ * @brief 删除指定数据库下的表
+ * @details 当前未实现此功能，直接返回UNIMPLEMENTED错误码
+ * @param dbname 数据库名称
+ * @param relation_name 表名
+ * @return RC 始终返回UNIMPLEMENTED错误码
+ */
 RC DefaultHandler::drop_table(const char *dbname, const char *relation_name) { return RC::UNIMPLEMENTED; }
 
+/**
+ * @brief 查找已打开的数据库
+ * @details 在已打开的数据库集合中查找指定名称的数据库
+ * @param dbname 数据库名称
+ * @return Db* 找到返回数据库指针，未找到返回nullptr
+ */
 Db *DefaultHandler::find_db(const char *dbname) const
 {
   map<string, Db *>::const_iterator iter = opened_dbs_.find(dbname);
@@ -153,29 +239,47 @@ Db *DefaultHandler::find_db(const char *dbname) const
   return iter->second;
 }
 
+/**
+ * @brief 查找指定数据库中的表
+ * @details 先查找数据库，然后在数据库中查找表
+ * @param dbname 数据库名称
+ * @param table_name 表名
+ * @return Table* 找到返回表指针，未找到或参数错误返回nullptr
+ */
 Table *DefaultHandler::find_table(const char *dbname, const char *table_name) const
 {
+  // 参数有效性检查
   if (dbname == nullptr || table_name == nullptr) {
     LOG_WARN("Invalid argument. dbname=%p, table_name=%p", dbname, table_name);
     return nullptr;
   }
+  
+  // 查找数据库
   Db *db = find_db(dbname);
   if (nullptr == db) {
     return nullptr;
   }
 
+  // 在数据库中查找表
   return db->find_table(table_name);
 }
 
+/**
+ * @brief 同步所有打开的数据库
+ * @details 将所有打开的数据库的数据同步到磁盘，如果任一数据库同步失败则立即返回错误
+ * @return RC 操作结果状态码，成功返回SUCCESS，任一数据库同步失败返回相应错误码
+ */
 RC DefaultHandler::sync()
 {
   RC rc = RC::SUCCESS;
+  
+  // 遍历所有已打开的数据库并同步
   for (const auto &db_pair : opened_dbs_) {
     Db *db = db_pair.second;
     rc     = db->sync();
     if (rc != RC::SUCCESS) {
       LOG_ERROR("Failed to sync db. name=%s, rc=%d:%s", db->name(), rc, strrc(rc));
-      return rc;
+      return rc;  ///< 任一数据库同步失败，立即返回错误
     }
   }
   return rc;
--- src/observer/storage/default/default_handler.h
+++ src/observer/storage/default/default_handler.h
@@ -13,88 +13,131 @@ See the Mulan PSL v2 for more details. */
 //
 #pragma once
 
-#include "storage/db/db.h"
-#include "common/lang/span.h"
-#include "common/lang/map.h"
-#include "common/lang/string.h"
-#include "common/lang/memory.h"
+#include "storage/db/db.h"               ///< 包含数据库类定义
+#include "common/lang/span.h"           ///< 包含span类型定义
+#include "common/lang/map.h"            ///< 包含map容器扩展
+#include "common/lang/string.h"         ///< 包含字符串处理函数
+#include "common/lang/memory.h"         ///< 包含内存管理相关函数
 
-class Trx;
-class TrxKit;
+class Trx;     ///< 前向声明事务类
+class TrxKit;  ///< 前向声明事务工具类
 
 /**
- * @brief 数据库存储引擎的入口
- * @details 参考MySQL，可以抽象handler层，作为SQL层与存储层交互的接口。但是当前还不具备这个条件。
+ * @brief 默认存储引擎处理器
+ * @details 数据库存储引擎的核心入口类，作为SQL层与存储层交互的主要接口。
+ * 负责数据库的创建、删除、打开、关闭等基本操作，以及表的创建和删除等元数据管理。
+ * 参考MySQL的handler层设计思想，但当前实现较为基础。
  */
 class DefaultHandler
 {
 public:
+  /**
+   * @brief 构造函数
+   * @details 初始化DefaultHandler的基本成员变量
+   */
   DefaultHandler();
 
+  /**
+   * @brief 析构函数
+   * @details 自动调用destroy()方法释放所有资源
+   */
   virtual ~DefaultHandler() noexcept;
 
   /**
    * @brief 初始化存储引擎
-   * @param base_dir 存储引擎的根目录。所有的数据库相关数据文件都放在这个目录下
-   * @param trx_kit_name 使用哪种类型的事务模型
-   * @param log_handler_name 使用哪种类型的日志处理器
+   * @details 设置存储目录，初始化事务模型和日志处理器，创建系统数据库
+   * @param base_dir 存储引擎的根目录，所有数据库相关数据文件都放在这个目录下
+   * @param trx_kit_name 事务模型名称，指定使用的事务处理方式
+   * @param log_handler_name 日志处理器名称，指定使用的日志处理方式
+   * @param storage_engine 存储引擎名称，指定使用的存储引擎类型
+   * @return RC 操作结果状态码，成功返回SUCCESS，失败返回相应错误码
    */
   RC   init(const char *base_dir, const char *trx_kit_name, const char *log_handler_name, const char *storage_engine);
+  
+  /**
+   * @brief 销毁存储引擎
+   * @details 同步所有数据到磁盘，释放所有打开的数据库资源
+   */
   void destroy();
 
   /**
-   * @brief 创建一个数据库
-   * @details 在路径base_dir下创建一个名为dbname的空库，生成相应的系统文件。
-   * @param dbname 数据库名称
+   * @brief 创建数据库
+   * @details 在存储目录下创建一个名为dbname的数据库目录
+   * @param dbname 数据库名称，不能为空或空白字符串
+   * @return RC 操作结果状态码，成功返回SUCCESS，数据库已存在返回SCHEMA_DB_EXIST，参数错误返回INVALID_ARGUMENT
    */
   RC create_db(const char *dbname);
 
   /**
    * @brief 删除数据库
-   * @details 当前并没有实现
+   * @details 当前尚未实现此功能
    * @param dbname 数据库名称
+   * @return RC 当前返回INTERNAL错误码
    */
   RC drop_db(const char *dbname);
 
   /**
-   * @brief 打开一个数据库
-   * @param dbname 数据库名称
+   * @brief 打开数据库
+   * @details 加载数据库信息到内存，初始化数据库相关组件
+   * @param dbname 数据库名称，不能为空或空白字符串
+   * @return RC 操作结果状态码，成功返回SUCCESS，数据库不存在返回SCHEMA_DB_NOT_EXIST
    */
   RC open_db(const char *dbname);
 
   /**
-   * @brief 关闭指定数据库。
-   * @details 该操作将关闭当前数据库中打开的所有文件，关闭文件操作将自动使所有相关的缓冲区页面更新到磁盘
+   * @brief 关闭指定数据库
+   * @details 该操作将关闭数据库中打开的所有文件，将缓冲区页面更新到磁盘
+   * @param dbname 数据库名称
+   * @return RC 当前返回UNIMPLEMENTED，功能未实现
    */
   RC close_db(const char *dbname);
 
   /**
-   * @brief 在指定的数据库下创建一个表
+   * @brief 在指定数据库下创建表
    * @param dbname 数据库名称
    * @param relation_name 表名
-   * @param attributes 属性信息
+   * @param attributes 属性信息数组，定义表的结构
+   * @return RC 操作结果状态码，成功返回SUCCESS，数据库未打开返回SCHEMA_DB_NOT_OPENED
    */
   RC create_table(const char *dbname, const char *relation_name, span<const AttrInfoSqlNode> attributes);
 
   /**
    * @brief 删除指定数据库下的表
-   * @details 当前没有实现。需要删除表在内存中和磁盘中的所有资源，包括表的索引文件。
+   * @details 当前没有实现此功能，需要删除表在内存中和磁盘中的所有资源
    * @param dbname 数据库名称
    * @param relation_name 表名
+   * @return RC 当前返回UNIMPLEMENTED，功能未实现
    */
   RC drop_table(const char *dbname, const char *relation_name);
 
 public:
+  /**
+   * @brief 查找已打开的数据库
+   * @param dbname 数据库名称
+   * @return Db* 找到返回数据库指针，未找到返回nullptr
+   */
   Db    *find_db(const char *dbname) const;
+  
+  /**
+   * @brief 查找指定数据库中的表
+   * @param dbname 数据库名称
+   * @param table_name 表名
+   * @return Table* 找到返回表指针，未找到或参数错误返回nullptr
+   */
   Table *find_table(const char *dbname, const char *table_name) const;
 
+  /**
+   * @brief 同步所有打开的数据库
+   * @details 将所有打开的数据库的数据同步到磁盘
+   * @return RC 操作结果状态码，成功返回SUCCESS，任一数据库同步失败返回相应错误码
+   */
   RC sync();
 
 private:
-  filesystem::path  base_dir_;          ///< 存储引擎的根目录
-  filesystem::path  db_dir_;            ///< 数据库文件的根目录
-  string            trx_kit_name_;      ///< 事务模型的名称
-  string            log_handler_name_;  ///< 日志处理器的名称
-  map<string, Db *> opened_dbs_;        ///< 打开的数据库
-  string            storage_engine_;    ///< 存储引擎的名称
+  filesystem::path  base_dir_;          ///< 存储引擎的根目录路径
+  filesystem::path  db_dir_;            ///< 数据库文件的根目录路径，通常为base_dir_/db
+  string            trx_kit_name_;      ///< 事务模型的名称，用于创建事务管理器
+  string            log_handler_name_;  ///< 日志处理器的名称，用于创建日志管理器
+  map<string, Db *> opened_dbs_;        ///< 存储所有已打开的数据库，键为数据库名，值为数据库指针
+  string            storage_engine_;    ///< 存储引擎的名称，指定使用的存储实现
 };

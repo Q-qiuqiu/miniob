75e154e22c9642da585942968731812c567b27fe
xinhai12310 <18135082@qq.com>
2025-10-18 03:53:09 -0700

    聚合函数实现

--- src/observer/sql/expr/aggregator.cpp
+++ src/observer/sql/expr/aggregator.cpp
@@ -15,22 +15,42 @@ See the Mulan PSL v2 for more details. */
 #include "sql/expr/aggregator.h"
 #include "common/log/log.h"
 
+/**
+ * @brief 累计处理一个输入值到总和中
+ * @details 将输入值累加到聚合器内部存储的中间结果中。如果是第一个输入值，则直接赋值；
+ * 否则，执行加法运算，将当前值与中间结果相加。
+ * @param[in] value 要累加到总和中的输入值
+ * @return 操作结果状态码，成功返回RC::SUCCESS
+ * @retval RC::SUCCESS 累加成功
+ */
 RC SumAggregator::accumulate(const Value &value)
 {
+  // 检查是否为第一次累加操作
   if (value_.attr_type() == AttrType::UNDEFINED) {
+    // 第一次累加，直接将输入值赋给中间结果
     value_ = value;
     return RC::SUCCESS;
   }
   
+  // 确保输入值类型与中间结果类型一致，否则会导致计算错误
   ASSERT(value.attr_type() == value_.attr_type(), "type mismatch. value type: %s, value_.type: %s", 
         attr_type_to_string(value.attr_type()), attr_type_to_string(value_.attr_type()));
   
+  // 执行加法操作，将输入值加到中间结果中
   Value::add(value, value_, value_);
   return RC::SUCCESS;
 }
 
+/**
+ * @brief 获取计算得到的总和结果
+ * @details 将聚合器内部存储的累加结果复制到输出参数中。对于求和操作，直接返回累计的中间结果即可。
+ * @param[out] result 用于存储总和结果的值
+ * @return 操作结果状态码，成功返回RC::SUCCESS
+ * @retval RC::SUCCESS 获取结果成功
+ */
 RC SumAggregator::evaluate(Value& result)
 {
+  // 将内部存储的累加结果赋值给输出参数
   result = value_;
   return RC::SUCCESS;
 }
--- src/observer/sql/expr/aggregator.h
+++ src/observer/sql/expr/aggregator.h
@@ -12,26 +12,65 @@ See the Mulan PSL v2 for more details. */
 // Created by Wangyunlai on 2024/05/29.
 //
 
+/**
+ * @file aggregator.h
+ * @brief 聚合函数计算模块
+ * @details 该文件定义了聚合函数的基类和具体实现类，用于处理SQL中的聚合操作（如SUM、AVG、MIN、MAX等）。
+ * 聚合器负责累计计算聚合结果，并在需要时返回最终的聚合值。
+ */
+
 #pragma once
 
 #include "common/value.h"
 #include "common/sys/rc.h"
 
+/**
+ * @brief 聚合器基类
+ * @details 定义了所有聚合函数实现类需要遵循的接口规范。聚合器的主要功能是累计处理输入值，并在需要时输出聚合结果。
+ */
 class Aggregator
 {
 public:
+  /**
+   * @brief 析构函数
+   * @details 虚析构函数，确保派生类对象能够正确析构
+   */
   virtual ~Aggregator() = default;
 
+  /**
+   * @brief 累计处理一个输入值
+   * @param[in] value 要累计的输入值
+   * @return 操作结果状态码，成功返回RC::SUCCESS
+   */
   virtual RC accumulate(const Value &value) = 0;
   virtual RC evaluate(Value &result)        = 0;
 
 protected:
+  /**
+   * @brief 存储聚合中间结果的值
+   * @details 不同类型的聚合器会以不同方式使用该值来存储中间计算结果
+   */
   Value value_;
 };
 
+/**
+ * @brief 求和聚合器
+ * @details 实现SUM聚合函数，用于计算一组数值的总和
+ */
 class SumAggregator : public Aggregator
 {
 public:
+  /**
+   * @brief 累计处理一个输入值到总和中
+   * @param[in] value 要累加到总和中的输入值
+   * @return 操作结果状态码，成功返回RC::SUCCESS
+   */
   RC accumulate(const Value &value) override;
+  
+  /**
+   * @brief 获取计算得到的总和结果
+   * @param[out] result 用于存储总和结果的值
+   * @return 操作结果状态码，成功返回RC::SUCCESS
+   */
   RC evaluate(Value &result) override;
 };
